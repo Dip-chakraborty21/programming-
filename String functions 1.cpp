9595c9177ececcabe33355b99f6861ec106eeec4
Dip-chakraborty21 <98321912+Dip-chakraborty21@users.noreply.github.com>
2022-01-28 12:54:32 +0530

    Create String functions 1.cpp

---
+++ String functions 1.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+ string s = "chakraborty";
+    cout << s.front()<< endl; 
+    cout << s.back()<< endl; 
+    return 0;
+}
