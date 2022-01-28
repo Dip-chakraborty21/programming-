187e82105ddd709bb56fc92fcfc97945e4609bdd
Dip-chakraborty21 <98321912+Dip-chakraborty21@users.noreply.github.com>
2022-01-28 12:24:32 +0530

    Create String functions.cpp

---
+++ String functions.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string.h> 
+using namespace std; 
+int main() {
+string s="Dipchakraborty";
+string s1;
+s1 = s.substr(5,8);
+    cout << s1;
+
+    return 0;
+}
