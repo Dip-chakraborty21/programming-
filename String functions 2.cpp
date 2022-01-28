7914d6ef0e1f0c3056c1211ae7a5d59c48196e9c
Dip-chakraborty21 <98321912+Dip-chakraborty21@users.noreply.github.com>
2022-01-28 13:05:44 +0530

    Create String functions 2.cpp

---
+++ String functions 2.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<string.h>
+using namespace std; 
+int main() {
+    string a= "all";
+    string b= "cat";
+    cout << a.compare(b);
+
+    return 0;
+}
